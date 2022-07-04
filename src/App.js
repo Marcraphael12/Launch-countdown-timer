86f8efddf46ced11fde999f5ff7903cbf3d211a4
Marc raphael <marcraphael979@gmail.com>
2022-07-04 15:31:49 +0100

    App: initiate an interval

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   const [setMinutes, setMinutesState] = useState();
   const [setSeconds, setSecondsState] = useState();
 
+  let interval ;
+  
   return (
     <div className="App">
      <Clock setDays={setDays} setHours={setHours} setMinutes={setMinutes} setSeconds={setSeconds} />
