548660074e6cadb2a11857104e3e8f819e60d65c
Marc raphael <marcraphael979@gmail.com>
2022-07-04 14:45:02 +0100

    components/clock: create the clock structure

--- src/components/clock.js
+++ src/components/clock.js
@@ -2,9 +2,31 @@ import React, {Fragment} from 'react';
 
 const Clock = ({setDays, setHours, setMinutes, setSeconds}) => {
 	return (
-		<div>
-			clock
-		</div>
+		<Fragment>
+			<div className="clock-container">
+				<div className="clock">
+					<div className="days">
+						<p>{setDays}</p>
+						<h3>Days</h3>
+					</div>
+
+					<div className="hours">
+						<p>{setHours}</p>
+						<h3>Hours</h3>
+					</div>
+
+					<div className="minutes">
+						<p>{setMinutes}</p>
+						<h3>Minutes</h3>
+					</div>
+
+					<div className="seconds">
+						<p>{setSeconds}</p>
+						<h3>Seconds</h3>
+					</div>
+				</div>
+			</div>
+		</Fragment>
 	)
 }
 
