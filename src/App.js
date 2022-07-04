242a4a7534b90388fb5e758200d9560616c12cf7
Marc raphael <marcraphael979@gmail.com>
2022-07-04 14:25:47 +0100

    App: define states for days, hours, minutes, seconds

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.scss';
 import Clock from './components/clock';
 
 function App() {
+  const [setDays, setDaysState] = useState();
+  const [setHours, setHoursState] = useState();
+  const [setMinutes, setMinutesState] = useState();
+  const [setSeconds, setSecondsState] = useState();
+
   return (
     <div className="App">
      <Clock />
